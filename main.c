ba8b99e7241315ffff8007700215a573566b6682
charynepesov <65546416+charynepesov@users.noreply.github.com>
2022-03-24 20:36:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 
 int main() {
     // to store the execution time of code
-    double time_spent = 0.0;
-
+    double time_spent = 0.0; //degisken
     clock_t begin = clock();
-    // do some stuff here
+    // çalışma zamanını ölçmek istediğimiz fonsiyonu burada çağırıyoruz 
 
     int sayi=0;
     printf("lutfen bir ile 100 arasi sayi giriniz");
@@ -16,11 +15,10 @@ int main() {
     printf("%d\n", sayi2);
 
     clock_t end = clock();
-
-    // calculate elapsed time by finding difference (end - begin) and
-    // dividing the difference by CLOCKS_PER_SEC to convert to seconds
+ 
+    // (end - begin) den çıkartdığımızda çalışma zamanını buluruz.
+    // saniye ye çevirmek için CLOCKS_PER_SEC değişkenine böleriz
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-
     printf("The elapsed time is %f seconds", time_spent);
 
     return 0;
